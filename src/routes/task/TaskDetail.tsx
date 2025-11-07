ae1cbc00a52cd9ef2433511e416b9a16d198625e
TYrueouJeL <guerinremi.pro@gmail.com>
2025-11-07 08:58:47 +0100

    Fix react-router

--- src/routes/task/TaskDetail.tsx
+++ src/routes/task/TaskDetail.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dpù";
+import { useLoaderData } from "react-router-dom";
 import TaskDetail from "../../components/task/TaskDetail.tsx";
 import {getTask} from "../../services/api.ts";
 import {useEffect, useState} from "react";
